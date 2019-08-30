c0b4bbc4b41603fbcba6c64eb4a73835708a4c00
Marco Sabatini <marco.sabatini@external-rumbo.com>
2019-08-30 15:11:01 +0200

    added scala project for the kata

---
+++ scala/src/test/scala/VideoStoreTests.scala
@@ -0,0 +1,9 @@
+import org.scalatest.{FlatSpec, Inside, Matchers}
+
+class VideoStoreTests extends FlatSpec with Matchers with Inside {
+
+  "true" should "be true" in {
+
+    true shouldBe true
+  }
+}
