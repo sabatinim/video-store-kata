69abfc1a3119f7e477f802866f2f8d95a9afed55
Marco Sabatini <marco.sabatini@external-rumbo.com>
2019-08-30 15:50:03 +0200

    first exploration tests with a regular movie.

--- scala/src/test/scala/VideoStoreTests.scala
+++ scala/src/test/scala/VideoStoreTests.scala
@@ -2,8 +2,41 @@ import org.scalatest.{FlatSpec, Inside, Matchers}
 
 class VideoStoreTests extends FlatSpec with Matchers with Inside {
 
-  "true" should "be true" in {
+  case class NewReleaseMovie(title: String, price: String = "3.0")
 
-    true shouldBe true
+  case class User(name: String)
+
+  def receiptFor(u: User, m: NewReleaseMovie, days: Int): String = {
+
+    val newPrice = m.price.toDouble * days
+    "Rental Record for " + u.name + "\n" +
+      "- " + m.title + " " + newPrice +
+      "You owed " + newPrice + "\n" +
+      "You earned 0 frequent renter points"
+
   }
+
+  it should "cost 3 EUR rent a new release movie for one day" in {
+
+    val actualReceipt = receiptFor(User("A_USER"), NewReleaseMovie("TITLE"),1)
+
+    actualReceipt shouldBe
+      "Rental Record for A_USER\n" +
+        "- TITLE 3.0" +
+        "You owed 3.0\n" +
+        "You earned 0 frequent renter points"
+  }
+
+  it should "cost 6.0 EUR rent a new release movie for two day" in {
+
+    val actualReceipt = receiptFor(User("A_USER"), NewReleaseMovie("TITLE"), 2)
+
+    actualReceipt shouldBe
+      "Rental Record for A_USER\n" +
+        "- TITLE 6.0" +
+        "You owed 6.0\n" +
+        "You earned 0 frequent renter points"
+  }
+
+
 }
