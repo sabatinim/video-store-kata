65230646a27341300efe857be9202d84e820da89
Marco Sabatini <marco.sabatini@external-rumbo.com>
2019-09-06 14:33:13 +0200

    some refactoring. Now I'm using a function in order to get the price.

--- scala/src/test/scala/VideoStoreTests.scala
+++ scala/src/test/scala/VideoStoreTests.scala
@@ -2,32 +2,27 @@ import org.scalatest.{FlatSpec, Inside, Matchers}
 
 class VideoStoreTests extends FlatSpec with Matchers with Inside {
 
-  case class NewReleaseMovie(title: String, price: String = "3.0")
+  trait Movie
+
+  case class NewReleaseMovie(title: String, price: String = "3.0") extends Movie
 
   case class User(name: String)
 
-  def receiptFor(u: User, m: NewReleaseMovie, days: Int): String = {
+  def receiptFor(u: User, m: NewReleaseMovie, days: Int)(implicit priceFun: Movie => String): String = {
 
-    val newPrice = m.price.toDouble * days
+    val newPrice = priceFun(m).toDouble * days
     "Rental Record for " + u.name + "\n" +
       "- " + m.title + " " + newPrice +
       "You owed " + newPrice + "\n" +
       "You earned 0 frequent renter points"
-
   }
 
-  it should "cost 3 EUR rent a new release movie for one day" in {
-
-    val actualReceipt = receiptFor(User("A_USER"), NewReleaseMovie("TITLE"),1)
-
-    actualReceipt shouldBe
-      "Rental Record for A_USER\n" +
-        "- TITLE 3.0" +
-        "You owed 3.0\n" +
-        "You earned 0 frequent renter points"
-  }
 
   it should "cost 6.0 EUR rent a new release movie for two day" in {
+
+    implicit val priceFor: Movie => String = {
+      case NewReleaseMovie(_, price) => price
+    }
 
     val actualReceipt = receiptFor(User("A_USER"), NewReleaseMovie("TITLE"), 2)
 
@@ -37,6 +32,4 @@ class VideoStoreTests extends FlatSpec with Matchers with Inside {
         "You owed 6.0\n" +
         "You earned 0 frequent renter points"
   }
-
-
 }
