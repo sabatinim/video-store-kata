d00a8b3fd3b3894bd3e64e53a5e8153ecfe2d04f
msabatini <marco.sabatini@latminute.com>
2019-10-09 12:24:09 +0200

    some refactoring.

--- typescript/app/__test__/VideoStoreTest.ts
+++ typescript/app/__test__/VideoStoreTest.ts
@@ -2,6 +2,25 @@
 
 describe('It works', function () {
 
+    class MoviePrices {
+        additionalCost: number;
+        movieBasePrice: number;
+        constructor(additionalCost: number, movieBasePrice: number) {
+            this.additionalCost = additionalCost;
+            this.movieBasePrice = movieBasePrice;
+        }
+    }
+
+    class Rental {
+        rentalDays: number;
+        m: Movie;
+        constructor(rentalDays: number, m: Movie) {
+            this.rentalDays = rentalDays;
+            this.m = m;
+        }
+
+    }
+
     interface Movie{
         price():number
 
@@ -38,38 +57,35 @@ describe('It works', function () {
         }
     }
 
-    function additionalCostFor(days: number,minRentDay: number, additionalCostPerDay: any) {
+    let additionalCostFor = (rental: Rental):MoviePrices => {
         let additionalCost = 0.0;
-        if (days > minRentDay) {
-            const additionalDays = days - minRentDay
-            additionalCost = additionalCostPerDay * additionalDays;
+        if (rental.rentalDays > rental.m.minRentDays()) {
+            const additionalDays = rental.rentalDays - rental.m.minRentDays()
+            additionalCost = rental.m.additionaCostPerDay() * additionalDays;
         }
-        return additionalCost;
+        return new MoviePrices(additionalCost,rental.m.price());
     }
 
-    let priceFor = (m: Movie,days:number): number => {
-
-        return m.price() + additionalCostFor(days,m.minRentDays(), m.additionaCostPerDay())
+    let priceFor = (moviePrices: MoviePrices): number => {
+        return moviePrices.movieBasePrice + moviePrices.additionalCost
     };
 
     it('rent new release movie one day', () => {
-        expect(priceFor(new NewReleaseMovie(),1)).toEqual( 3.0)
+
+        expect(priceFor(additionalCostFor(new Rental(1,new NewReleaseMovie())))).toEqual( 3.0)
+
     });
 
     it('rent new release movie two day', () => {
-        expect(priceFor(new NewReleaseMovie(),2)).toEqual( 6.0)
+        expect(priceFor(additionalCostFor(new Rental(2,new NewReleaseMovie())))).toEqual( 6.0)
     });
 
     it('rent children movie one day', () => {
-        expect(priceFor(new ChildrenMovie(),1)).toEqual( 1.5)
+        expect(priceFor(additionalCostFor(new Rental(1, new ChildrenMovie())))).toEqual( 1.5)
     });
 
-    it('rent children movie one day', () => {
-        expect(priceFor(new ChildrenMovie(),2)).toEqual( 1.5)
-    });
-
-    it('rent children movie one day', () => {
-        expect(priceFor(new ChildrenMovie(),4)).toEqual( 3.0)
+    it('rent children movie four day', () => {
+        expect(priceFor( additionalCostFor(new Rental(4,new ChildrenMovie())))).toEqual( 3.0)
     });
 
 
