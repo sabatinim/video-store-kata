ae60cc5adb1289529becfcf59e9cd3ce4294a311
msabatini <marco.sabatini@latminute.com>
2019-10-09 14:41:05 +0200

    now we're using composition in order to calculate movie price.

--- typescript/app/__test__/VideoStoreTest.ts
+++ typescript/app/__test__/VideoStoreTest.ts
@@ -1,10 +1,9 @@
-
-
 describe('It works', function () {
 
     class MoviePrices {
         additionalCost: number;
         movieBasePrice: number;
+
         constructor(additionalCost: number, movieBasePrice: number) {
             this.additionalCost = additionalCost;
             this.movieBasePrice = movieBasePrice;
@@ -14,6 +13,7 @@ describe('It works', function () {
     class Rental {
         rentalDays: number;
         m: Movie;
+
         constructor(rentalDays: number, m: Movie) {
             this.rentalDays = rentalDays;
             this.m = m;
@@ -21,17 +21,18 @@ describe('It works', function () {
 
     }
 
-    interface Movie{
-        price():number
+    interface Movie {
+        price(): number
 
         minRentDays(): number;
 
         additionaCostPerDay(): number;
     }
-    class NewReleaseMovie implements Movie{
+
+    class NewReleaseMovie implements Movie {
         public basePrice = 3.0;
 
-        price():number {
+        price(): number {
             return this.basePrice
         }
 
@@ -43,6 +44,7 @@ describe('It works', function () {
             return 3.0;
         }
     }
+
     class ChildrenMovie implements Movie {
         price(): number {
             return 1.5;
@@ -57,37 +59,61 @@ describe('It works', function () {
         }
     }
 
-    let additionalCostFor = (rental: Rental):MoviePrices => {
+    let additionalCostFor = (rental: Rental): MoviePrices => {
         let additionalCost = 0.0;
         if (rental.rentalDays > rental.m.minRentDays()) {
             const additionalDays = rental.rentalDays - rental.m.minRentDays()
             additionalCost = rental.m.additionaCostPerDay() * additionalDays;
         }
-        return new MoviePrices(additionalCost,rental.m.price());
+        return new MoviePrices(additionalCost, rental.m.price());
     }
 
     let priceFor = (moviePrices: MoviePrices): number => {
         return moviePrices.movieBasePrice + moviePrices.additionalCost
     };
 
+    let compose = <TX,TY,R>(
+        f: (x: TX) => TY,
+        g: (y: TY) => R):
+        (x: TX) => R => {
+        return (x) => g(f(x))
+    }
+
+
+    it('compose two function', () => {
+
+        let f = (x: string): string => {
+            return `f(${x})`
+        }
+        let g = (x: string): string => {
+            return `g(${x})`
+        }
+
+
+
+
+        let gfx: (x: string) => string = compose(f, g)
+
+        expect(gfx("value")).toEqual("g(f(value))")
+    });
+
+    let moviePriceFor: (x: Rental) => number = compose(additionalCostFor,priceFor)
+
     it('rent new release movie one day', () => {
 
-        expect(priceFor(additionalCostFor(new Rental(1,new NewReleaseMovie())))).toEqual( 3.0)
+        expect(moviePriceFor(new Rental(1, new NewReleaseMovie()))).toEqual(3.0)
 
     });
 
     it('rent new release movie two day', () => {
-        expect(priceFor(additionalCostFor(new Rental(2,new NewReleaseMovie())))).toEqual( 6.0)
+        expect(moviePriceFor(new Rental(2, new NewReleaseMovie()))).toEqual(6.0)
     });
 
     it('rent children movie one day', () => {
-        expect(priceFor(additionalCostFor(new Rental(1, new ChildrenMovie())))).toEqual( 1.5)
+        expect(moviePriceFor(new Rental(1, new ChildrenMovie()))).toEqual(1.5)
     });
 
     it('rent children movie four day', () => {
-        expect(priceFor( additionalCostFor(new Rental(4,new ChildrenMovie())))).toEqual( 3.0)
+        expect(moviePriceFor(new Rental(4, new ChildrenMovie()))).toEqual(3.0)
     });
-
-
-
 });
