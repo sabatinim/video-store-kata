f7744fd3d61af60dcdcd353166c1ebb4335ed0c9
msabatini <marco.sabatini@latminute.com>
2020-08-24 15:39:58 +0200

    refactoring.

--- typescript/app/source/domain/movie/htmlReceipt.ts
+++ typescript/app/source/domain/movie/htmlReceipt.ts
@@ -8,20 +8,18 @@ const htmlMovieReceipt = (m: PrintableMovie): string =>
     `<li>${m.title} ${m.priceRepresentation}</li>`
 
 const htmlMoviesReceiptWith = (
-    htmlMovieReceipt: (x: Rental) => string):
-    (rentals: Rental[]) => string =>
-    (rentals) => `<ul>\n${rentals.map(r => htmlMovieReceipt(r)).join("\n")}\n</ul>`
+    htmlMovieReceipt: (x: Rental) => string) =>
+    (rentals: Rental[]) => `<ul>\n${rentals.map(r => htmlMovieReceipt(r)).join("\n")}\n</ul>`
 
 const htmlFooterReceiptWith = (
-    calculateMoviesTotalPrice: (rentals: Rental[]) => number):
-    (rentals: Rental[]) => string =>
-    (rentals) => `<br>You owed ${calculateMoviesTotalPrice(rentals).toPrecision(2)}`
+    calculateMoviesTotalPrice: (rentals: Rental[]) => number) =>
+    (rentals: Rental[]) => `<br>You owed ${calculateMoviesTotalPrice(rentals).toPrecision(2)}`
 
 const htmlFooterRentalPointReceiptWith = (
-    calculateRentalPoint: (rentals: Rental[]) => number):
-    (rentals: Rental[]) => string =>
-    (rentals) => `<br>You earned ${calculateRentalPoint(rentals)} frequent renter points\n</body>\n</html>`
+    calculateRentalPoint: (rentals: Rental[]) => number) =>
+    (rentals: Rental[]) => `<br>You earned ${calculateRentalPoint(rentals)} frequent renter points\n</body>\n</html>`
 
+//WIRING HERE
 const htmlFooterRentalPointReceipt: (rentals: Rental[]) => string =
     htmlFooterRentalPointReceiptWith(calculateRentalPoints);
 
@@ -42,6 +40,7 @@ const htmlHeader = (user: string) =>
     `<body>\n` +
     `<h1>Rental Record for ${user}</h1>\n`
 
+//WIRING THE PRINT FUNCTION WITH HTML TEXT BEHAVIOUR
 export const printHtmlReceipt: (user: string, rentals: Rental[]) => string =
     genericReceipt(
         htmlHeader,
--- typescript/app/source/domain/movie/textReceipt.ts
+++ typescript/app/source/domain/movie/textReceipt.ts
@@ -8,20 +8,18 @@ const textMovieReceipt = (m: PrintableMovie): string =>
      `- ${m.title} ${m.priceRepresentation}`
 
 const textMoviesReceiptWith = (
-    movieReceiptFunc: (x: Rental) => string):
-    (rentals: Rental[]) => string =>
-     (rentals) => rentals.map(r => movieReceiptFunc(r)).join("\n")
+    movieReceiptFunc: (x: Rental) => string) =>
+     (rentals: Rental[]) => rentals.map(r => movieReceiptFunc(r)).join("\n")
 
 const textFooterReceiptWith = (
-    totalPrice: (rentals: Rental[]) => number):
-    (rentals: Rental[]) => string =>
-     (rentals) => `Total ${totalPrice(rentals).toPrecision(2)}`
+    totalPrice: (rentals: Rental[]) => number) =>
+     (rentals: Rental[]) => `Total ${totalPrice(rentals).toPrecision(2)}`
 
 const textFooterRentalPointReceiptWith = (
-    calculateRentalPoint: (rentals: Rental[]) => number):
-    (rentals: Rental[]) => string =>
-     (rentals) => `Total Rental points ${calculateRentalPoint(rentals)}`
+    calculateRentalPoint: (rentals: Rental[]) => number) =>
+     (rentals: Rental[]) => `Total Rental points ${calculateRentalPoint(rentals)}`
 
+//WIRING HERE
 const textFooterRentalPointReceipt =
     textFooterRentalPointReceiptWith(calculateRentalPoints);
 
@@ -34,6 +32,8 @@ const textMoviesReceipt: (rentals: Rental[]) => string =
         textMovieReceipt))
 
 const textHeader = (user: string) => `Hello ${user} this is your receipt\n`;
+
+//WIRING THE PRINT FUNCTION WITH PLAIN TEXT BEHAVIOUR
 export const printTextReceipt: (user: string, rentals: Rental[]) => string =
     genericReceipt(
         textHeader,
