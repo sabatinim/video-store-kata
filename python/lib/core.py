2f2ecd2d8093e1394cb59f5407ee23581ac6fda3
Marco Sabatini <marco.sabatini@nozominetworks.com>
2022-03-09 11:49:42 +0100

    test python

---
+++ python/lib/core.py
@@ -0,0 +1,45 @@
+from decimal import *
+
+
+class Movie:
+
+    def __init__(self, rent_price, max_rent_days, additional_cost_per_day):
+        self.rent_price = rent_price
+        self.max_rent_days = max_rent_days
+        self.additional_cost_per_day = additional_cost_per_day
+
+    def price(self, how_many_days: int) -> Decimal:
+        return Decimal(self.rent_price).__add__(self.__additional_cost_for(how_many_days))
+
+    def __additional_cost_for(self, how_many_days):
+        additional_cost = Decimal(0.0)
+        if how_many_days > self.max_rent_days:
+            additional_days = how_many_days - self.max_rent_days
+            additional_cost = Decimal(self.additional_cost_per_day).__mul__(Decimal(additional_days))
+        return additional_cost
+
+
+class Rent:
+
+    def __init__(self, how_many_days: int, m: Movie):
+        self._how_many_days = how_many_days
+        self._movie = m
+
+    def price_for(self) -> Decimal:
+        return self._movie.price(self._how_many_days)
+
+
+def regular_movie() -> Movie:
+    return Movie(
+        rent_price=2.0,
+        max_rent_days=2,
+        additional_cost_per_day=1.5
+    )
+
+
+def base_movie() -> Movie:
+    return Movie(
+        rent_price=3.0,
+        max_rent_days=1,
+        additional_cost_per_day=3.0
+    )
---
+++ python/test/test_app.py
@@ -0,0 +1,25 @@
+import unittest
+
+from lib.core import Rent, regular_movie, base_movie
+
+
+class AppTests(unittest.TestCase):
+
+    def test_rent_regular_movie(self):
+        self.__assert_movie({
+            1: 2.0, 2: 2.0,
+            3: 3.5, 4: 5.0,
+            5: 6.5, 6: 8.0
+        }, regular_movie())
+
+    def test_rent_base_movie(self):
+        self.__assert_movie({
+            1: 3.0, 2: 6.0,
+            3: 9.0, 4: 12.0,
+            5: 15.0, 6: 18.0
+        }, base_movie())
+
+    def __assert_movie(self, expected, movie):
+        for k, v in expected.items():
+            p = Rent(k, movie).price_for()
+            assert (p == v)
