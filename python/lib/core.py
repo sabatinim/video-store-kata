a4bcb5e936c542bc0f3220b8261b085ab57a4612
Marco Sabatini <marco.sabatini@nozominetworks.com>
2022-03-09 14:47:26 +0100

    add total cost test

--- python/lib/core.py
+++ python/lib/core.py
@@ -3,7 +3,10 @@ from decimal import *
 
 class Movie:
 
-    def __init__(self, rent_price, max_rent_days, additional_cost_per_day):
+    def __init__(
+            self, rent_price, max_rent_days,
+            additional_cost_per_day
+    ):
         self.rent_price = rent_price
         self.max_rent_days = max_rent_days
         self.additional_cost_per_day = additional_cost_per_day
@@ -29,6 +32,15 @@ class Rent:
         return self._movie.price(self._how_many_days)
 
 
+class RentGroup:
+
+    def __init__(self, *rents):
+        self.rents = rents
+
+    def total_price(self) -> Decimal:
+        return sum(map(lambda r: r.price_for(), self.rents))
+
+
 def regular_movie() -> Movie:
     return Movie(
         rent_price=2.0,
@@ -42,4 +54,12 @@ def base_movie() -> Movie:
         rent_price=3.0,
         max_rent_days=1,
         additional_cost_per_day=3.0
+    )
+
+
+def children_movie() -> Movie:
+    return Movie(
+        rent_price=1.5,
+        max_rent_days=3,
+        additional_cost_per_day=1.5
     )
--- python/test/test_movie.py
+++ python/test/test_movie.py
@@ -1,6 +1,6 @@
 import unittest
 
-from lib.core import Rent, regular_movie, base_movie
+from lib.core import Rent, regular_movie, base_movie, children_movie, RentGroup
 
 
 class MovieTests(unittest.TestCase):
@@ -18,6 +18,21 @@ class MovieTests(unittest.TestCase):
             3: 9.0, 4: 12.0,
             5: 15.0, 6: 18.0
         }, base_movie())
+
+    def test_rent_children_movie(self):
+        self.__assert_movie({
+            1: 1.5, 2: 1.5,
+            3: 1.5, 4: 3.0,
+            5: 4.5, 6: 6.0
+        }, children_movie())
+
+    def test_rent_multiple_type(self):
+        total = RentGroup(
+            Rent(4, children_movie()),
+            Rent(4, base_movie()),
+            Rent(4, regular_movie())
+        ).total_price()
+        assert (total == 20.0)
 
     def __assert_movie(self, expected, movie):
         for k, v in expected.items():
